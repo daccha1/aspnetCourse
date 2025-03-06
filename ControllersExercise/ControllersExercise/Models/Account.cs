155da13ba9e852bff784b4c1b27b3146d2b42da8
daccha <nijedavid@gmail.com>
2025-03-06 10:27:06 +0100

    Controller-BankApp-Assignment

---
+++ ControllersExercise/ControllersExercise/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿namespace ControllersExercise.Models
+{
+    public class Account
+    {
+        public int accountNumber {  get; set; }
+        public string accountHolderName { get; set; }
+        public int currentBalance { get; set; }
+        
+        public Account()
+        {
+            accountNumber = 1001;
+            accountHolderName = "ClientName";
+            currentBalance = 5000;
+        }
+    
+    }
+}
